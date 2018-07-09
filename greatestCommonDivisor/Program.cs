cb3c84d045bd9d8222346735a2dae34dc3427414
sirisirisiri <ammatasiriyako@gmail.com>
2018-07-09 08:47:04 +0530

    wdd

---
+++ greatestCommonDivisor/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace greatestCommonDivisor
+{
+    class Program
+    {
+        public int gcd(int x,int y)
+        {
+            if (x % y == 0)
+            {
+                return y;
+            }
+
+            else
+            {
+                return gcd(y, x % y);
+            }
+ 
+        }
+         
+        static void Main(string[] args)
+        {
+            Program t = new Program();
+            int x = t.gcd(12, 16);
+             System.Console.WriteLine(x);
+            System.Console.Read();
+        }
+    }
+}
